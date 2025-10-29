06496c89e4cdaa8913313e588f02549905145644
Ali Nihat Puytu <alinihatpuytu@gmail.com>
2025-10-29 19:02:17 +0300

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import HomePage from './pages/Home'
 import ProjectsPage from './pages/ProjectsPage'
 import ContactPage from './pages/ContactPage'
+
+function AnalyticsTracker() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.va?.('pageview')
+  }, [location.pathname, location.search])
+
+  return null
+}
 
 export default function App() {
   const theme = useSelector(s => s.theme.mode)
@@ -16,6 +26,7 @@ export default function App() {
 
   return (
     <BrowserRouter>
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
